3726771500cca9bdf687037d96a3188ffd914b47
YvanCitron <yvan.lefevre@etudiant.univ-rennes2.fr>
2024-03-15 14:45:47 +0100

    dataframes de base

--- marketing_yvan.py
+++ marketing_yvan.py
@@ -1,3 +1,11 @@
 from datetime import datetime
 import numpy as np
 import pandas as pd
+
+indiv = pd.read_csv('donnee/R_COMPLEMENT_INDIVIDU_2016.csv',delimiter = ";",encoding="iso-8859-1",dtype={'EAN':'str'})
+compl = pd.read_csv('donnee/R_INDIVIDU_2016.csv',delimiter = ";",encoding="iso-8859-1",dtype={'EAN':'str'})
+
+
+DF_indiv = pd.DataFrame(indiv)
+DF_compl = pd.DataFrame(compl)
+DF_fusion = pd.merge(DF_indiv,DF_compl)
