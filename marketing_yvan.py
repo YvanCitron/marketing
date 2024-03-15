8457982c98e7ff84c55678caada4fc3a32d83893
YvanCitron <yvan.lefevre@etudiant.univ-rennes2.fr>
2024-03-15 15:08:10 +0100

    commit yvan, merge

--- marketing_yvan.py
+++ marketing_yvan.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 R_complement_individu_2016=pd.read_csv("donnee/R_COMPLEMENT_INDIVIDU_2016.csv",delimiter=";",encoding= 'iso-8859-1',dtype={'EAN':'str'})
 R_individu_2016=pd.read_csv("donnee/R_INDIVIDU_2016.csv",delimiter=";",encoding= 'iso-8859-1',dtype={'EAN':'str'})
-R_magasin_2016=pd.read_csv("donnee/R_MAGASIN_2016.csv",delimiter=";",encoding= 'iso-8859-1',dtype={'EAN':'str'})
+R_magasin_2016=pd.read_csv("donnee/R_MAGASIN.csv",delimiter=";",encoding= 'iso-8859-1',dtype={'EAN':'str'})
 R_referentiel=pd.read_csv("donnee/R_REFERENTIEL.csv",delimiter=";",encoding= 'iso-8859-1',dtype={'EAN':'str'})
 R_tickets_2016=pd.read_csv("donnee/R_TICKETS_2016.csv",delimiter=";",encoding= 'iso-8859-1',dtype={'EAN':'str'})
 R_typo_produit=pd.read_csv("donnee/R_TYPO_PRODUIT.csv",delimiter=";",encoding= 'iso-8859-1',dtype={'EAN':'str'})
@@ -18,4 +18,5 @@ print(R_typo_produit)
 
 DF_indiv = pd.DataFrame(R_individu_2016)
 DF_compl = pd.DataFrame(R_complement_individu_2016)
-DF_fusion = pd.merge(DF_indiv,DF_compl)
+DF_fusion = pd.merge(DF_indiv,DF_compl,on='ID_INDIVIDU',how='left')
+DF_fusion
