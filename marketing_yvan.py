8276b0a79291fe63088e95c56be36a456d390c04
MathurinG <mathurin.gesny@etudiant.univ-rennes2.fr>
2024-03-15 16:00:15 +0100

    anciennete

--- marketing_yvan.py
+++ marketing_yvan.py
@@ -18,7 +18,13 @@ print(R_typo_produit)
 
 
 DF_fusion = pd.merge(R_individu_2016,R_complement_individu_2016,on='INDIVIDUS_ID',how='left')
-
+print(type(DF_fusion.iloc[1]['DATE_CREATION_CARTE']))
 DF_fusion['date_naissance']=pd.to_datetime(DF_fusion[['DATE_NAISS_A','DATE_NAISS_M','DATE_NAISS_J']].astype(str).apply(lambda x: '-'.join(x),axis=1),format='%Y-%m-%d',errors='coerce')
 DF_fusion['AGE']=(datetime.datetime.strptime('2016-08-31','%Y-%m-%d')-DF_fusion['date_naissance'])//datetime.timedelta(days=365.2425)
+DF_fusion['Anciennete']=[(datetime.datetime.strptime('2016-08-31','%Y-%m-%d'))-datetime.datetime.strptime(i,'%d/%m/%Y') for i in DF_fusion['DATE_CREATION_CARTE']]
 print(DF_fusion)
+
+print(R_tickets_2016.columns)
+print(R_magasin.columns)
+print(R_referentiel.columns)
+print(R_typo_produit.columns)
