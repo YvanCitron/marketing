7c18ae1f47c91c9af3549e7ec97932959e589328
MathurinG <mathurin.gesny@etudiant.univ-rennes2.fr>
2024-03-15 14:46:53 +0100

    import des jeu de données

--- marketing_yvan.py
+++ marketing_yvan.py
@@ -1,3 +1,16 @@
 from datetime import datetime
 import numpy as np
 import pandas as pd
+R_complement_individu_2016=pd.read_csv("donnee/R_COMPLEMENT_INDIVIDU_2016.csv",delimiter=";",encoding= 'iso-8859-1',dtype={'EAN':'str'})
+R_individu_2016=pd.read_csv("donnee/R_INDIVIDU_2016.csv",delimiter=";",encoding= 'iso-8859-1',dtype={'EAN':'str'})
+R_magasin_2016=pd.read_csv("donnee/R_MAGASIN_2016.csv",delimiter=";",encoding= 'iso-8859-1',dtype={'EAN':'str'})
+R_referentiel=pd.read_csv("donnee/R_REFERENTIEL.csv",delimiter=";",encoding= 'iso-8859-1',dtype={'EAN':'str'})
+R_tickets_2016=pd.read_csv("donnee/R_TICKETS_2016.csv",delimiter=";",encoding= 'iso-8859-1',dtype={'EAN':'str'})
+R_typo_produit=pd.read_csv("donnee/R_TYPO_PRODUIT.csv",delimiter=";",encoding= 'iso-8859-1',dtype={'EAN':'str'})
+
+print(R_complement_individu_2016)
+print(R_individu_2016)
+print(R_magasin_2016)
+print(R_referentiel)
+print(R_tickets_2016)
+print(R_typo_produit)
